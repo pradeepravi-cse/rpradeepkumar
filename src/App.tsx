c573ae3a66502e83132ae2d82bb5b572060799ee
pradeepkumar <pradeepravi.cse@gmail.com>
2022-01-30 07:51:45 +0530

    backing up svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import { Home } from './Modules';
+import React from "react";
+import { Home } from "./Modules";
 
-import './Styles/index.scss';
-import 'antd/dist/antd.min.css';
-import { Navbar } from './Components';
+import "./Styles/index.scss";
+import "antd/dist/antd.min.css";
+import { Navbar } from "./Components";
+import Signature from "./Assets/signature.svg";
+import "./App.scss";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home />
+      <Navbar />
+      {/* <Home /> */}
+      <div
+        style={{
+          position: "fixed",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          background: "black",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Signature />
+      </div>
     </>
   );
 }
