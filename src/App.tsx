b1a8369e972292c2194d02100c43efc6685e8361
pradeepkumar <pradeepravi.cse@gmail.com>
2022-01-30 09:34:53 +0530

    ejected

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,15 @@ import { Home } from "./Modules";
 import "./Styles/index.scss";
 import "antd/dist/antd.min.css";
 import { Navbar } from "./Components";
-import Signature from "./Assets/signature.svg";
+import { Signature } from "./Components/Signature";
 import "./App.scss";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Home /> */}
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-          background: "black",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+      <Home />
+      <div className="SVG-wrapper">
         <Signature />
       </div>
     </>
